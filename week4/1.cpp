0d41525e5c336b4089fb6b046b5903e13804d734
Beisenbek Baisakov <beysenbek@gmail.com>
2023-09-30 11:08:27 +0600

    'upd'

--- week4/1.cpp
+++ week4/1.cpp
@@ -27,6 +27,9 @@ struct bst{
         bool find(int val){
             return find(root, val) != NULL;
         }
+        void del(int val){
+            root = del(root, val);
+        }
 
     private:
         node * root;
@@ -59,6 +62,42 @@ struct bst{
             return find(current->left, val);
         }
 
+        node * rightmost(node * current){
+            if(current == NULL) return NULL;
+            if(current->right == NULL) return current;
+            return rightmost(current->right);
+        }
+
+        node * del(node * current, int val){
+            if(current == NULL) return NULL;
+            if(current->val == val){
+                if(current->left == NULL && current->right == NULL){
+                    delete current;
+                    return NULL;
+                }else if(current->left == NULL && current->right != NULL){
+                    node * temp = current->right;
+                    delete current;
+                    return temp;
+                }else if(current->left != NULL && current->right == NULL){
+                    node * temp = current->left;
+                    delete current;
+                    return temp;
+                }else if(current->left != NULL && current->right != NULL){
+                    node * temp = rightmost(current->left);
+                    if(temp != NULL){
+                        current->val = temp->val;
+                    }
+                    current->left = del(current->left, current->val);
+                }
+            }else if(current->val > val){
+                current->left = del(current->left, val);
+            }
+            else if(current->val < val){
+                current->right = del(current->right, val);
+            }
+            return current;
+        }
+
 };
 
 int main(){
@@ -73,10 +112,16 @@ int main(){
 
     b->print();
     cout << endl;
-
+    /*
     for(int i = 0; i <= 10; ++i){
         cout << i << " " << b->find(i) << endl;
     }
+    cout << endl;
+    */
+    int x;
+    cin >> x;
+    b->del(x);
+    b->print();
 
     return 0;
 }
